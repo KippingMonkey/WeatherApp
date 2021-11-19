ba8f021d0a2899f35692fc77d2815035ac6104b7
Jonas <jonas@kippingmonkey.se>
2021-11-19 23:49:42 +0100

    finish displayCityWeatherCard()

--- main.js
+++ main.js
@@ -1,82 +1,91 @@
-// /* GLOBAL VARIABELS
-// –––––––––––––––––––––––––––––––––––––––––––––––––– */
-// let city = "";
-// let apiKey = "";
-// let cities = [];
+/* GLOBAL VARIABELS
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+let city = "";
+let apiKey = "";
+let cities = [];
 
-// /* PROGRAM
-// –––––––––––––––––––––––––––––––––––––––––––––––––– */
-// clearOnRefresh();
+/* PROGRAM
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+clearOnRefresh();
 
-// /* EVENT HANDLERS
-// –––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* EVENT HANDLERS
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
 
-// $("#api-form").submit((e) => {
-//   e.preventDefault();
-//   apiKey = $("#input-key").val();
-//   $("#input-key").val("");
-//   alert(`apiKey = ${apiKey}`);
-// });
+$("#api-form").submit((e) => {
+  e.preventDefault();
+  apiKey = $("#input-key").val();
+  // $("#input-key").val("");
+  // alert(`apiKey = ${apiKey}`);
+});
 
-// $("#search-form").submit((e) => {
-//   e.preventDefault();
-//   if (apiKey === "") {
-//     alert("You must enter a api key");
-//     $("#input-city").val("");
-//   } else {
-//     city = $("#input-city").val();
-//     $("#input-city").val("");
-//     alert(`apiKey = ${apiKey} and city = ${city}`);
-//     checkInputAgainstArray();
-//     let result = getWeatherData();
-//     displayCityWeatherCard(result);
-//   }
-// });
+$("#search-form").submit((e) => {
+  e.preventDefault();
+  if (apiKey === "") {
+    alert("You must enter a api key");
+    $("#input-city").val("");
+  } else {
+    city = $("#input-city").val();
+    $("#input-city").val("");
+    // alert(`apiKey = ${apiKey} and city = ${city}`);
+    checkInputAgainstArray();
+    let result = getWeatherData();
+    displayCityWeatherCard(result);
+  }
+});
 
-// /* FUNCTIONS
-// –––––––––––––––––––––––––––––––––––––––––––––––––– */
-// function clearOnRefresh() {
-//   $("#input-key").val("");
-//   $("#input-city").val("");
-//   $("li").remove();
-//   cities = [];
-// }
+/* FUNCTIONS
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+function clearOnRefresh() {
+  $("#input-key").val("");
+  $("#input-city").val("");
+  $("li").remove();
+  cities = [];
+}
 
-// function checkInputAgainstArray() {
-//   if (cities.length > 0) {
-//     if (cities.includes(city)) {
-//       $(".message").text(
-//         "You already know what the weather is like there. You can also try to be more specifik in your search."
-//       );
-//       $("#input-city").val("");
-//     }
-//   } else {
-//     cities.push(city);
-//   }
-// }
+function checkInputAgainstArray() {
+  if (cities.length > 0) {
+    if (cities.includes(city)) {
+      $(".message").text(
+        "You already know what the weather is like there. You can also try to be more specifik in your search."
+      );
+      $("#input-city").val("");
+    }
+  } else {
+    cities.push(city);
+  }
+}
 
-// async function getWeatherData() {
-//   const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+async function getWeatherData() {
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
-//   const params = new URLSearchParams({
-//     q: city,
-//     appid: apiKey,
-//     units: "metric",
-//   });
+  const params = new URLSearchParams({
+    q: city,
+    appid: apiKey,
+    units: "metric",
+  });
 
-//   let url = baseUrl + params.toString();
+  let url = baseUrl + params.toString();
 
-//   const response = await fetch(url);
-//   const json = await response.json();
+  const response = await fetch(url);
+  const json = await response.json();
 
-//   return json;
-// }
+  return json;
+}
 
-// async function displayCityWeatherCard(result) {
-//   let forecast = await result;
-//   console.log(forecast);
-//   const markup = `<li>${forecast.weather[0].icon} ${forecast.weather[0].description}
-//   </li>`;
+async function displayCityWeatherCard(result) {
+  let forecast = await result;
+  console.log(forecast);
+  const markup = `<li>
+                <div class="result-card">
+                    <h2><span>${city}</span><sup class="sup-city">${forecast.sys.country}</sup></h2>
+                    <div class="city-temp">${Math.round(forecast.main.temp)}<sup class="sup-celcius">°C</sup></div>
+                    <figure>
+                        <img class="city-icon" src="icons/${forecast.weather[0].icon}.png" alt="weathericon">
+                        <figcaption>${forecast.weather[0].description}</figcaption>
+                        <p>Feels like ${Math.round(forecast.main.feels_like)} °C</p>
+                    </figure>
+                </div>
+            </li>`;
 
-//   $(".result-list").append(markup);
-// }
+  $(".results").append(markup);
+}
